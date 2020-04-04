f2716d40d7ab4ae5b1ae27500dd44cf98e75182c
hammadNS96 <hammad96@outlook.com>
2020-04-04 18:33:52 +0000

    Made initial Database queries

---
+++ db/schema/06_menu_items.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS items CASCADE;
+
+CREATE TABLE menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  menu_id INTEGER REFERENCES menu_categories(id) ON DELETE CASCADE,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  prep_time SMALLINT,
+  cost INTEGER NOT NULL
+);
