1536e249352897146045eb5a303cf48e92072de6
hammadNS96 <hammad96@outlook.com>
2020-04-07 16:39:06 +0000

    Updated the order_items and items table

--- db/schema/07_order_items.sql
+++ db/schema/07_order_items.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS order_items CASCADE;
 CREATE TABLE order_items (
   id SERIAL PRIMARY KEY NOT NULL,
   order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
-  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE
+  total_price INTEGER REFERENCES price(id) ON DELETE CASCADE,
+  total_menu_items INTEGER REFERENCES menu_items(id) ON DELETE CASCADE 
 );
--- db/seeds/06_items.sql
+++ db/seeds/06_items.sql
@@ -1,22 +1,22 @@
-INSERT INTO menu_items (
-  menu_id, name, description, prep_time, cost
+INSERT INTO _items (
+  menu_id, name, description, prep_time, cost, quantity 
 ) VALUES (
-  2, 'Veggie Burger', 'Shiitake mushroom, organic teriyaki sauce, organic tamari, aïoli sauce, sriracha coleslaw', 15, 20
+  2, 'Veggie Burger', 'Shiitake mushroom, organic teriyaki sauce, organic tamari, aïoli sauce, sriracha coleslaw', 15, 20, 5
 ), (
-  2, 'Salad', 'with non-GMO general tao sauce. Edamame salad, kimchi, lettuce, beets, carrot, broccoli, organic sprouted mung beans, spiced chickpeas, non-GMO goji berries, black sesame seeds', 10, 7
+  2, 'Salad', 'with non-GMO general tao sauce. Edamame salad, kimchi, lettuce, beets, carrot, broccoli, organic sprouted mung beans, spiced chickpeas, non-GMO goji berries, black sesame seeds', 10, 7, 6
 ), (
-  2, 'Bombay Seasonal Soup', 'Filtered water, red lentils, sweet potato, coconut milk, onions, non-GMO canola oil, sea salt, ginger, garlic, potato starch, chili flakes ', 5, 20
+  2, 'Bombay Seasonal Soup', 'Filtered water, red lentils, sweet potato, coconut milk, onions, non-GMO canola oil, sea salt, ginger, garlic, potato starch, chili flakes ', 5, 20, 7
 ), (
-  2, 'Sweet Potato', 'Oven-baked French Fries hand-cut in-store, house spice, olive oil, lemon', 5, 9
+  2, 'Sweet Potato', 'Oven-baked French Fries hand-cut in-store, house spice, olive oil, lemon', 5, 9. 10
 ), (
-  2, 'Vegan Wrap', 'Whole wheat tortilla .Choice of vegetables. Optional Sauces', 10, 10
+  2, 'Vegan Wrap', 'Whole wheat tortilla .Choice of vegetables. Optional Sauces', 10, 10, 4
 ), (
-  2, 'Egg Salad Sandwich', 'Gluten free bread. Mayo and Lettuce. Omega 3 eggs', 5, 7.5
+  2, 'Egg Salad Sandwich', 'Gluten free bread. Mayo and Lettuce. Omega 3 eggs', 5, 7.5, 5
 ), (
-  2, 'Avacado Toast', 'Locally grown avaocados. Whole grain toast. Optional seasoning', 2, 1000
+  2, 'Avacado Toast', 'Locally grown avaocados. Whole grain toast. Optional seasoning', 2, 1000, 4
 ), (
-  3, 'Smoothie', 'Fresh organinc fruits. Fresh organic vegetables. Probiotics', 7, 12
+  3, 'Smoothie', 'Fresh organinc fruits. Fresh organic vegetables. Probiotics', 7, 12, 8
 ), (
-  3, 'Quinoa Salad', 'Fluffy Quinoa. Local chickpeas. Choice of vegetables ', 10, 1150
+  3, 'Quinoa Salad', 'Fluffy Quinoa. Local chickpeas. Choice of vegetables ', 10, 1150, 5
 ),
 );
